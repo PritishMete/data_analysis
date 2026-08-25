6d821c6b7b2ad8b6f01484b5b2f02c84ad676075
PritishMete <jaiphotoshoot@gmail.com>
2026-08-25 22:31:40 +0530

    Deploy fresh Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3878560911" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1387054609" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
