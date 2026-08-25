3cd91a16228e29dc47fde46a20b263de9156301a
PritishMete <jaiphotoshoot@gmail.com>
2026-08-25 22:37:15 +0530

    Deploy production web assets to gh-pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1387054609" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1561942161" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
