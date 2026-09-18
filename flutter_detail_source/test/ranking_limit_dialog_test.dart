71febc24b8055fd5e3e47694fb5b72a25b68862c
pritish mete <67995645+PritishMete@users.noreply.github.com>
2026-09-18 13:16:42 +0530

    Fix ranking selector test Material import

--- flutter_detail_source/test/ranking_limit_dialog_test.dart
+++ flutter_detail_source/test/ranking_limit_dialog_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../lib/widgets/overlays/ranking_limit_dialog.dart';
