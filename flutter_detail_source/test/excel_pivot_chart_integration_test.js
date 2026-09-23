07ec864cd77ab0bd833140e467aad2cdb04c92e6
pritish mete <67995645+PritishMete@users.noreply.github.com>
2026-09-23 20:47:07 +0530

    Fix PivotTable chart guard integration fixture

--- flutter_detail_source/test/excel_pivot_chart_integration_test.js
+++ flutter_detail_source/test/excel_pivot_chart_integration_test.js
@@ -55,6 +55,14 @@ function mock(){
 (async()=>{
   const {context,calls,pivotRange}=mock();
   vm.runInContext(fs.readFileSync(path.join(__dirname,'..','web','excel_authorization_guard.js'),'utf8'),context);
+  context.fetch = async () => ({status: 200});
+  context.window.insightflowSetMutationAuthorization(
+    'test-token',
+    'test-org',
+    'wc_test',
+    'excel.mutate.working_copy',
+    'https://authz.test',
+  );
   vm.runInContext(fs.readFileSync(path.join(__dirname,'..','web','excel_native_chart.js'),'utf8'),context);
 
   const placement={
